ec13e13a0558994076029da1678de5ead5ddeebb
chimaeberechiphilip <114532255+chimaeberechiphilip@users.noreply.github.com>
2025-01-03 18:56:28 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,31 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import react from "@vitejs/plugin-react";
+import { sveltekit } from '@sveltejs/kit/vit';
 
 // https://vite.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
 // })
 
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "http://localhost:3001",
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// });
+
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3001",
-        changeOrigin: true,
-      },
-    },
-  },
-});
+   plugins: [sveltekit()],
+   server: {
+     watch:{
+       usePolling:true
+     },
+     host:true
+   },
+ });
+
